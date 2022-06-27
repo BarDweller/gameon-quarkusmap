772f1beab09d8f2208957ba7b0e8276985fc41c8
BarDweller <bardweller@gmail.com>
2022-06-27 14:51:07 -0400

    swap rest client, add missing couch class for native

--- src/main/java/org/gameontext/map/model/QuarkusNativeReflectionRegistration.java
+++ src/main/java/org/gameontext/map/model/QuarkusNativeReflectionRegistration.java
@@ -2,7 +2,7 @@ package org.gameontext.map.model;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection(ignoreNested = false, classNames = {"org.ektorp.impl.QueryResultParser"})
+@RegisterForReflection(ignoreNested = false, classNames = {"org.ektorp.impl.QueryResultParser", "org.ektorp.ComplexKey"})
 public class QuarkusNativeReflectionRegistration {
     
 }
